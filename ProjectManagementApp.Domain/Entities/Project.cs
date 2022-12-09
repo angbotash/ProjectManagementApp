368d21a8f895672293e7a5a69623a99acc9810cc
Angela Botasheva <angbotash@gmail.com>
2022-12-09 19:26:49 +0600

    Add Project.cs

---
+++ ProjectManagementApp.Domain/Entities/Project.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManagementApp.Domain.Entities
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ClientCompanyName { get; set; }
+        public string ExecutorCompanyName { get; set; }
+        public ICollection<Employee> Employees { get; set; }
+        public Employee TeamLeader { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int Priority { get; set; }
+    }
+}
