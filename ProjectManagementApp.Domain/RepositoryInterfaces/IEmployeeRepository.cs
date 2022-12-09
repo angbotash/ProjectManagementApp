8d60e6dd81e2b9c0611d7e17feedad53b0b653f0
Angela Botasheva <angbotash@gmail.com>
2022-12-09 22:01:05 +0600

    Add IEmployeeRepository

---
+++ ProjectManagementApp.Domain/RepositoryInterfaces/IEmployeeRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjectManagementApp.Domain.Entities;
+
+namespace ProjectManagementApp.Domain.RepositoryInterfaces
+{
+    public interface IEmployeeRepository
+    {
+        public void Create(Employee newEmployee);
+        public void Update(Employee updatedEmployee);
+        public Employee GetById(int id);
+        public void Delete(int id);
+    }
+}
